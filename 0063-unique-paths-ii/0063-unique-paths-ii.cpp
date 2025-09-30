edf70a721093ec48c9a8fd963b86e16c8311de6d
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2025-09-30 19:36:19 +0530

    Time: 3 ms (11.43%), Space: 11.9 MB (55.06%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -13,9 +13,10 @@ public:
         return dp[m][n] = up+left;
     }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        if(obstacleGrid[0][0] == 1) return 0;
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
-        if( m == 1 && n == 1 && obstacleGrid[0][0] == 1) return 0;
+        
         vector<vector<int>>dp(m,vector<int>(n,-1));
         return helper(obstacleGrid,m-1,n-1,dp);
     }
