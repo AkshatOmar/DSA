629c3595bb045938f64c55ed5a24000bb6f1e62f
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2025-10-05 02:20:45 +0530

    Time: 85 ms (10.5%), Space: 21 MB (28.47%) - LeetHub

--- 1559-cherry-pickup-ii/1559-cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/1559-cherry-pickup-ii.cpp
@@ -1,29 +1,41 @@
 class Solution {
 public:
-    int helper(vector<vector<int>>&grid,int i, int j1, int j2,int n,int m,vector<vector<vector<int>>>&dp) {
-        if(j1<0 || j1 >m-1 || j2<0 || j2>m-1) {
-            return -1e9;
-        }
-        if(i == n-1) {
-            if(j1 == j2) return grid[i][j1];
-            else return grid[i][j1]+grid[i][j2];
-        }
-        if(dp[i][j1][j2]!=-1) return dp[i][j1][j2];
-        int maxi = -1e9;
-        for(int d1 = -1;d1<=1;d1++) {
-            for(int d2 = -1;d2<=1;d2++) {
-                if(j1 == j2) maxi = max(maxi,grid[i][j1]+helper(grid,i+1,j1+d1,j2+d2,n,m,dp));
-                else {
-                    maxi = max(maxi,grid[i][j1]+grid[i][j2]+helper(grid,i+1,j1+d1,j2+d2,n,m,dp));
-                }
-            }
-        }
-        return dp[i][j1][j2] = maxi;
-    }
     int cherryPickup(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<vector<int>>>dp(n,vector<vector<int>>(m,vector<int>(m,-1)));
-        return helper(grid,0,0,m-1,n,m,dp);
+        vector<vector<vector<int>>>dp(n,vector<vector<int>>(m,vector<int>(m,0)));
+        //Base case
+        for(int j1 = 0;j1<m;j1++) {
+            for(int j2 = 0;j2<m;j2++){
+                if(j1 == j2) 
+                    dp[n-1][j1][j2] = grid[n-1][j1];
+                else
+                    dp[n-1][j1][j2] = grid[n-1][j1]+grid[n-1][j2];
+            }        
+        }
+        
+        
+        for(int i = n-2;i>=0;i--) {
+            for(int j1 = 0;j1<m;j1++) {
+                for(int j2 = 0;j2<m;j2++) {
+                    int maxi = -1e9;
+                    for(int d1 = -1;d1<=1;d1++) {
+                        for(int d2 = -1;d2<=1;d2++) {
+
+                            if(j1 == j2){
+                                if(j1+d1<m && j2+d2<m && j1+d1>=0 && j2+d2>=0)
+                                maxi = max(maxi,grid[i][j1]+dp[i+1][j1+d1][j2+d2]);
+                            } 
+                            else {
+                                if(j1+d1<m && j2+d2<m && j1+d1>=0 && j2+d2>=0)
+                                maxi = max(maxi,grid[i][j1]+grid[i][j2]+dp[i+1][j1+d1][j2+d2]);
+                            }
+                        }
+                    }
+                    dp[i][j1][j2] = maxi;
+                }
+            }
+        }
+        return dp[0][0][m-1];
     }
 };
