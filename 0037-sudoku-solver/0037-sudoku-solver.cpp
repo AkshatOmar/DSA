20fb59fce8549f30496a0821bd05b71002aab029
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2025-10-06 02:07:21 +0530

    Time: 308 ms (57.68%), Space: 8.6 MB (89.39%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,40 +1,43 @@
 class Solution {
 public:
-    bool isValid(vector<vector<char>>& board,int row, int col, char d) {
-        //validating row
+    bool isValid(vector<vector<char>>&board, char c, int row, int col) {
+        // not in row & col
         for(int i = 0;i<9;i++) {
-            if(board[row][i] == d) return false;
-            if(board[i][col] ==d) return false;
+            if(board[row][i] == c) return false;
+            if(board[i][col] == c) return false;
         }
-        int start_i = row/3*3;
-        int start_j = col/3*3;
-        for(int  k = 0;k<3;k++) {
-            for(int l = 0;l<3;l++) {
-                if(board[start_i +k][start_j + l] == d) {
-                    return false;
-                }
+        // not in 3x3 grid
+        // how to know which grid is this out of 9 3x3 grids
+        int startRow = row/3*3;
+        int startCol = col/3*3;
+        for(int i = 0;i<3;i++) {
+            for(int j = 0;j<3;j++) {
+                if(board[startRow+i][startCol+j] == c) return false;
             }
         }
         return true;
     }
-    bool solve(vector<vector<char>>& board) {
-        for(int i = 0;i<9;i++) {
-            for(int j = 0;j<9;j++) {
-                if(board[i][j] == '.') {
-                    for(char k = '1';k<='9';k++) {
-                        if(isValid(board,i,j,k)){
-                            board[i][j] = k;
-                            if(solve(board)) return true;
-                            board[i][j] = '.';
+    bool helper(vector<vector<char>>&board) {
+        for(int row = 0; row<9;row++) {
+            for(int col = 0;col<9;col++) {
+                if(board[row][col] == '.'){
+                    for( char c = '1'; c<='9'; c++) {
+                        if(isValid(board, c, row,col)){
+                            board[row][col] = c;
+                            if(helper(board))return true;
+                            board[row][col] = '.';
                         }
+                        
                     }
                     return false;
                 }
+                
             }
         }
         return true;
     }
     void solveSudoku(vector<vector<char>>& board) {
-        solve(board);
+        helper(board);
+         
     }
 };
