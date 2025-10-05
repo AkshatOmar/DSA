9fdef1a693d0a75368cbf7fb7e9d817cf80dfc15
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2025-10-06 03:14:11 +0530

    Time: 2 ms (76.17%), Space: 10 MB (91.85%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,44 +1,38 @@
 class Solution {
 public:
-    bool isValid(vector<string>&board, int row, int col,int n) {
+    bool isValid(vector<string>&temp,int row,int col,int n) {
         //upward
         for(int i = row-1;i>=0;i--) {
-            if(board[i][col] == 'Q') {
-                return false;
-            }
+            if(temp[i][col] == 'Q') return false;
         }
-        //left upward diagonal
-        for(int i = row-1,j = col-1;i>=0&&j>=0;i--,j--) {
-            if(board[i][j] == 'Q') 
-                return false;
+        //left diagonal
+        for(int i = row-1, j= col-1;i>=0&&j>=0;i--,j--) {
+            if(temp[i][j] == 'Q') return false;
         }
-        // right upward diagonal
-        for(int i=row-1,j = col+1;i>=0&&j<n;i--,j++) {
-            if(board[i][j] == 'Q')
-                return false;
+        //Right diagonal
+        for(int i = row-1, j= col+1;i>=0&&j<n;i--,j++) {
+            if(temp[i][j] == 'Q') return false;
         }
         return true;
     }
-    void find(int n,vector<vector<string>>&res,vector<string>&board,int row ) {
+    void helper(vector<vector<string>>&ans, int n, vector<string>&temp, int row) {
         if(row == n) {
-            res.push_back(board);
+            ans.push_back(temp);
             return;
         }
         for(int col = 0;col<n;col++) {
-            if(isValid(board,row,col,n)){
-                board[row][col] = 'Q';
-                find(n,res,board,row+1);
-                board[row][col] = '.';
+            if(isValid(temp,row,col,n)) {
+                temp[row][col] = 'Q';
+                helper(ans,n, temp, row+1);
+                temp[row][col] = '.';
             }
-            
         }
-            
         
     }
     vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>> res;
-        vector<string>board(n,string(n,'.'));
-        find(n, res,board,0);
-        return res;
+        vector<vector<string>>ans;
+        vector<string>temp(n,string(n,'.'));
+        helper(ans,n, temp ,0);
+        return ans;
     }
 };
