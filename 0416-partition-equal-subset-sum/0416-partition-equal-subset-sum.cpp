1e0a6644253970a7a48590ec7c4d3c130e3e6122
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2025-10-05 06:27:37 +0530

    Time: 142 ms (42.57%), Space: 17.3 MB (71.99%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,16 +1,5 @@
 class Solution {
 public:
-    bool helper(vector<int>&nums, int target,int idx,vector<vector<int>>&dp){
-        if(target == 0) return true;
-        if(idx == 0) return (nums[idx] == target);
-        if(dp[idx][target]!=-1) return dp[idx][target];
-        bool nopick = helper(nums,target,idx-1,dp);
-        bool pick = false;
-        if(target>=nums[idx]) {
-            pick = helper(nums,target-nums[idx],idx-1,dp);
-        }
-        return dp[idx][target] = pick||nopick;
-    }
     bool canPartition(vector<int>& nums) {
         int n = nums.size();
         int sum =0;
@@ -18,8 +7,22 @@ public:
             sum +=nums[i];
         }
         if(sum %2 == 0) {
-            vector<vector<int>>dp(n,vector<int>(sum/2+1,-1));
-            return helper(nums,sum/2,n-1,dp);
+            vector<vector<bool>>dp(n,vector<bool>(sum/2+1,0));
+            for(int i=0;i<n;i++) {
+                dp[i][0] = true;
+            }
+            if(nums[0]<=sum/2)dp[0][nums[0]] = false;
+            for(int i = 1;i<n;i++) {
+                for(int target = 1;target<=sum/2;target++) {
+                    bool nopick = dp[i-1][target];
+                    bool pick = false;
+                    if(target>=nums[i]) {
+                        pick = dp[i-1][target-nums[i]];
+                    }
+                    dp[i][target]=pick|nopick;
+                }
+            }
+            return dp[n-1][sum/2];
         }
         else return false;
     }
