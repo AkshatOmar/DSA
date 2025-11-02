bf6e536cc7e4278a254e49448b9f3a4e062328b1
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2025-11-02 15:18:22 +0530

    Time: 4 ms (80.42%), Space: 15.5 MB (48.84%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,19 +1,6 @@
 class Solution {
 public:
-    int helper(vector<int>&nums,int target,int idx,vector<vector<int>>&dp) {
-        if(idx == 0) {
-            if(target == 0 && nums[0] == 0) return 2;
-            if(target == 0 ||nums[0] == target) return 1;
-            return 0;
-        }
-        if(dp[idx][target] != -1) return dp[idx][target];
-        int nonpick = helper(nums,target,idx-1,dp);
-        int pick = 0;
-        if(nums[idx]<=target) {
-            pick = helper(nums,target-nums[idx],idx-1,dp);
-        }
-        return dp[idx][target] = pick + nonpick;
-    }
+  
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
         int totSum = 0;
@@ -22,7 +9,18 @@ public:
         }
         if((totSum+target)%2 != 0 || abs(target)>totSum)return 0;
         int T = (totSum+target)/2;
-        vector<vector<int>>dp(n,vector<int>(T+1,-1));
-        return helper(nums, T, n-1,dp);
+        vector<vector<int>>dp(n,vector<int>(T+1,0));
+        if(nums[0] == 0) dp[0][0] = 2;
+        else dp[0][0] = 1;
+        if(nums[0]!=0 && nums[0] <=T) dp[0][nums[0]] = 1;
+        for(int i = 1;i<n;i++) {
+            for(int sum = 0;sum<=T;sum++) {
+                int nonpick = dp[i-1][sum];
+                int pick = 0;
+                if(nums[i]<=sum) pick = dp[i-1][sum-nums[i]];
+                dp[i][sum] = pick+nonpick;
+            }
+        }
+        return dp[n-1][T];
     }
 };
