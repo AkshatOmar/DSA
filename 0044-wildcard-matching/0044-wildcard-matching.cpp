3448b1b2fc31f96631229a3e9a441c4f8310a667
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2025-11-09 11:34:02 +0530

    Time: 65 ms (13.48%), Space: 15.8 MB (50.59%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,32 +1,36 @@
 class Solution {
 public:
-    bool helper(string &s, string &p, int i, int j,vector<vector<int>>&dp) {
-        if(i<0 && j<0) {
-            return true;
-        }
-        if(j<0 && i>=0) {
-            return false;
-        }
-        if(i<0 && j>=0) {
-            for(int c = 0;c<=j;c++) {
-                if(p[c] != '*') return false;
-            }
-            return true;
-        }
-        if(dp[i][j] != -1) return dp[i][j];
-        bool x = false;
-        if(s[i] == p[j] || p[j] == '?') {
-            x = helper(s,p,i-1,j-1,dp);
-        }
-        else if(p[j] == '*') {
-            x = helper(s,p,i-1,j,dp)|helper(s,p,i,j-1,dp);
-        }
-        return dp[i][j] = x;
-    }
     bool isMatch(string s, string p) {
         int n = s.size();
         int m = p.size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return helper(s,p,n-1,m-1,dp);
+        vector<vector<bool>>dp(n+1,vector<bool>(m+1,false));
+        dp[0][0] = true;
+        //i>=0 && j<0
+        for(int i = 1;i<=n;i++) {
+            dp[i][0] = false;
+        }
+        //i<0 && j>=0
+        for(int j = 1;j<=m;j++) {
+            bool flag = true;
+            for(int j1 = 1;j1<=j;j1++) {
+                if(p[j1-1]!='*') {
+                    flag =false;
+                    break;
+                }
+            }
+            dp[0][j] =flag;
+        }
+        //dp table
+        for(int i =1;i<=n;i++) {
+            for(int j = 1;j<=m;j++) {
+                if(s[i-1] == p[j-1] || p[j-1] == '?') {
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                if(p[j-1] == '*') {
+                    dp[i][j] = dp[i-1][j] | dp[i][j-1];
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
