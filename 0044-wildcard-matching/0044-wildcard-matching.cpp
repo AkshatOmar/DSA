6e12f1c415f08ad3bd88392654b21e652c9ae0e7
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2025-11-09 12:27:23 +0530

    Time: 31 ms (73.3%), Space: 10.3 MB (85.81%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -3,34 +3,26 @@ public:
     bool isMatch(string s, string p) {
         int n = s.size();
         int m = p.size();
-        vector<vector<bool>>dp(n+1,vector<bool>(m+1,false));
-        dp[0][0] = true;
-        //i>=0 && j<0
-        for(int i = 1;i<=n;i++) {
-            dp[i][0] = false;
-        }
-        //i<0 && j>=0
+        vector<bool>prev(m+1,false);
+        vector<bool>curr(m+1,false);
+        prev[0] = true;
         for(int j = 1;j<=m;j++) {
-            bool flag = true;
-            for(int j1 = 1;j1<=j;j1++) {
-                if(p[j1-1]!='*') {
-                    flag =false;
-                    break;
-                }
-            }
-            dp[0][j] =flag;
+            if(p[j-1] == '*') prev[j] = prev[j-1];
         }
-        //dp table
         for(int i =1;i<=n;i++) {
+            curr[0] = false;
             for(int j = 1;j<=m;j++) {
                 if(s[i-1] == p[j-1] || p[j-1] == '?') {
-                    dp[i][j] = dp[i-1][j-1];
+                    curr[j] = prev[j-1];
                 }
-                if(p[j-1] == '*') {
-                    dp[i][j] = dp[i-1][j] | dp[i][j-1];
+                else if(p[j-1] == '*') {
+                    curr[j] = prev[j] | curr[j-1];
+                }else{
+                    curr[j] = false;
                 }
             }
+            prev = curr;
         }
-        return dp[n][m];
+        return prev[m];
     }
 };
