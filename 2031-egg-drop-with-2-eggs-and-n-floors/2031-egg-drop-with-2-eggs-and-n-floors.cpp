39db1aa132881fae8ba9f90b70960890db6455ab
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2025-11-14 03:32:48 +0530

    Time: 35 ms (25.59%), Space: 10.1 MB (20.85%) - LeetHub

--- 2031-egg-drop-with-2-eggs-and-n-floors/2031-egg-drop-with-2-eggs-and-n-floors.cpp
+++ 2031-egg-drop-with-2-eggs-and-n-floors/2031-egg-drop-with-2-eggs-and-n-floors.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
-    int helper(int e, int f,vector<vector<int>>&dp) {
-        if(f == 0 || f == 1) return f;
-        if(e == 1) return f;
-        if(dp[e][f] != -1) return dp[e][f];
-        int mini = 1e9;
-        for(int k = 1;k<=f;k++) {
-            int temp = 1+max(helper(e-1,k-1,dp),helper(e,f-k,dp));
-            mini = min(temp,mini);
-        }
-        return dp[e][f] = mini;
-    }
+    
     int twoEggDrop(int n) {
-        vector<vector<int>>dp(3,vector<int>(n+1,-1));
-        return helper(2,n,dp);
+        vector<vector<int>>dp(3,vector<int>(n+1,0));
+        
+        for(int f = 1;f<=n;f++) {
+            dp[1][f] = f;
+        }
+        for(int e = 2;e<=2;e++) {
+            dp[e][0] = 0;
+            dp[e][1] = 1;
+            for(int f = 2;f<=n;f++) {
+                int mini = 1e9;
+                for(int k = 1;k<=f;k++) {
+                    int maxAttempts = 1+ max(dp[e-1][k-1],dp[e][f-k]);
+                    mini = min(maxAttempts,mini);
+                }
+                dp[e][f] = mini;
+            }
+        }
+        return dp[2][n];
     }
 };
