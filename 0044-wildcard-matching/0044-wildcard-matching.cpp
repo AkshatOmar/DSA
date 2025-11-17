970ee34b8d67d3caf20ee42980de5095d5e7714d
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2025-11-18 03:56:40 +0530

    Time: 35 ms (63.74%), Space: 54.3 MB (14.08%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
+    bool helper(string &s, string &p, int i, int j,vector<vector<int>>&dp) {
+        if(i<0 && j<0) {
+            return true;
+        }
+        if(i>= 0 && j<0) return false;
+        if(i<0 && j>=0) {
+            for(int k = j;k>=0;k--) {
+                if(p[k] != '*') return false;
+            }
+            return true;
+        }
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(s[i] == p[j] || p[j] == '?') {
+            return dp[i][j] = helper(s,p,i-1,j-1,dp);
+        }
+        if(p[j] == '*') {
+            return dp[i][j] = helper(s,p,i-1,j,dp) || helper(s,p,i,j-1,dp);
+        }
+        return false;
+    }
     bool isMatch(string s, string p) {
         int n = s.size();
         int m = p.size();
-        vector<bool>prev(m+1,false);
-        vector<bool>curr(m+1,false);
-        prev[0] = true;
-        for(int j = 1;j<=m;j++) {
-            if(p[j-1] == '*') prev[j] = prev[j-1];
-        }
-        for(int i =1;i<=n;i++) {
-            curr[0] = false;
-            for(int j = 1;j<=m;j++) {
-                if(s[i-1] == p[j-1] || p[j-1] == '?') {
-                    curr[j] = prev[j-1];
-                }
-                else if(p[j-1] == '*') {
-                    curr[j] = prev[j] | curr[j-1];
-                }else{
-                    curr[j] = false;
-                }
-            }
-            prev = curr;
-        }
-        return prev[m];
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        return helper(s,p,n-1,m-1,dp);
     }
 };
