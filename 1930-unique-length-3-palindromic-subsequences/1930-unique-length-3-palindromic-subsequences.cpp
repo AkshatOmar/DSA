15d589f328754948a03ae208e7baf56da77f292b
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2025-11-22 02:10:42 +0530

    Time: 159 ms (55%), Space: 15.7 MB (84.77%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     int countPalindromicSubsequence(string s) {
+        // initialize 2 array for storing the occurance of each character
         int n = s.size();
-        vector<int>firstOcc(26,-1);
-        vector<int>lastOcc(26,-1);
+        vector<int>first(26,-1);
+        vector<int>last(26,-1);
         for(int i = 0;i<n;i++) {
             int c = s[i]-'a';
-            if(firstOcc[c]== -1) {
-                firstOcc[c] = i;
-            }
-            else{
-                lastOcc[c] = i;
+            if(first[c] == -1) {
+                first[c] = i;
+            }else {
+                last[c] = i;
             }
         }
         int ans = 0;
         for(int i = 0;i<26;i++) {
-            if(lastOcc[i]-firstOcc[i] >= 2){
+            if(last[i]-first[i] >= 2) {
                 unordered_set<int>st;
-                for(int j = firstOcc[i]+1; j<=lastOcc[i]-1; j++) {
+                for(int j = first[i]+1;j<=last[i]-1;j++) {
                     st.insert(s[j]);
                 }
-                ans += st.size();
+                ans+= st.size();
             }
         }
         return ans;
