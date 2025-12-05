d6db1abf9a0993d1c74a24528bf55a58485ca815
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2025-12-05 16:01:17 +0530

    Time: 0 ms (100%), Space: 22.4 MB (53.79%) - LeetHub

--- 3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.cpp
+++ 3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.cpp
@@ -5,13 +5,7 @@ public:
         for(int i =0;i<n;i++) {
             totSum +=nums[i];
         }
-        int left = 0, right = 0;
-        int cnt = 0;
-        for(int i =0;i<n-1;i++) {
-            left +=nums[i];
-            right = totSum-left;
-            if((left-right)%2 == 0) cnt++;
-        }
-        return cnt;
+        if(totSum %2 == 0) return n-1;
+        return 0;
     }
 };
