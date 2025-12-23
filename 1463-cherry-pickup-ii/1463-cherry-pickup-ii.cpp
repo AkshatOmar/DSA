bb7ace18afb5a8a16b431e1f562bccfb7282d62b
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2025-12-24 02:38:17 +0530

    Time: 47 ms (62.04%), Space: 21.1 MB (26.05%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
-    int helper(vector<vector<int>>&grid,int i, int j1, int j2,int n,int m,vector<vector<vector<int>>>&dp) {
-        if(j1<0 || j1 >m-1 || j2<0 || j2>m-1) {
-            return -1e9;
+
+    int helper(vector<vector<int>>& grid, int i1, int j1, int j2, int n, int m,vector<vector<vector<int>>>&dp) {
+        if(j1<0 || j1>m-1 || j2<0 || j2>m-1) return -1e9;
+        if(i1 == n-1) {
+            if(j1 == j2) {
+                return grid[i1][j1];
+            }
+            return grid[i1][j1]+grid[i1][j2];
         }
-        if(i == n-1) {
-            if(j1 == j2) return grid[i][j1];
-            else return grid[i][j1]+grid[i][j2];
-        }
-        if(dp[i][j1][j2]!=-1) return dp[i][j1][j2];
+        if(dp[i1][j1][j2]!=-1) return dp[i1][j1][j2];
         int maxi = -1e9;
         for(int d1 = -1;d1<=1;d1++) {
             for(int d2 = -1;d2<=1;d2++) {
-                if(j1 == j2) maxi = max(maxi,grid[i][j1]+helper(grid,i+1,j1+d1,j2+d2,n,m,dp));
-                else {
-                    maxi = max(maxi,grid[i][j1]+grid[i][j2]+helper(grid,i+1,j1+d1,j2+d2,n,m,dp));
+                if(j1 == j2) {
+                    maxi = max(maxi,grid[i1][j1]+helper(grid, i1+1,j1+d1,j2+d2,n,m,dp));
+                }
+                else{
+                    maxi = max(maxi,grid[i1][j1]+grid[i1][j2]+helper(grid,i1+1,j1+d1,j2+d2,n,m,dp));
                 }
             }
         }
-        return dp[i][j1][j2] = maxi;
+        return dp[i1][j1][j2] = maxi;
     }
     int cherryPickup(vector<vector<int>>& grid) {
         int n = grid.size();
