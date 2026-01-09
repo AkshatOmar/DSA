0ec9c58a63adcd321161abea5240147cf538a45e
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2026-01-09 06:01:51 +0530

    Time: 15 ms (75.92%), Space: 17.9 MB (41.63%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
-    
-    int helper(vector<int>&nums1,vector<int>&nums2,int i, int j,vector<vector<int>>&dp) {
-        if(i < 0 || j < 0) {
-            return -1e9;
-        }
-    if(dp[i][j] != -1) return dp[i][j];
-        int nopick1 = helper(nums1,nums2,i-1,j,dp);
-        int nopick2 = helper(nums1,nums2,i,j-1,dp);
-        int pick = max(nums1[i]*nums2[j],nums1[i]*nums2[j]+helper(nums1,nums2,i-1,j-1,dp));
-        return dp[i][j] = max({pick,nopick1,nopick2});
-    }
     int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size();
         int m = nums2.size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return helper(nums1,nums2,n-1,m-1,dp);
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int i = 0;i<=n;i++) {
+            dp[i][0] = -1e8;
+        }
+        for(int j = 0;j<=m;j++) {
+            dp[0][j] = -1e8;
+        }
+        for(int i = 1;i<=n;i++) {
+            for(int j = 1;j<=m;j++) {
+                int nopick1 = dp[i-1][j];
+                int nopick2 = dp[i][j-1];
+                int pick = max(nums1[i-1]*nums2[j-1],nums1[i-1]*nums2[j-1]+dp[i-1][j-1]);
+                dp[i][j] = max({pick,nopick1,nopick2});
+            }
+        }
+        return dp[n][m];
     }
 };
