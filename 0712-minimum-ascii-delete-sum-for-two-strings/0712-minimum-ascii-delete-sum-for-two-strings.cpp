2ce78915ff2cbf5799c14ec7ffc92029c24031a7
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2026-01-10 19:12:22 +0530

    Time: 16 ms (54.89%), Space: 18 MB (76.32%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -1,37 +1,26 @@
 class Solution {
 public:
-    int helper(string &s1, string &s2, int i, int j,vector<vector<int>>&dp) {
-        if(i<0) {
-            int remain = 0;
-            for(int j1 = j;j1>=0;j1--) {
-                remain+=s2[j1];
-            }
-            return remain;
-        }
-        if(j<0) {
-            int remain = 0;
-            for(int i1 = i;i1>=0;i1--) {
-                remain+=s1[i1];
-            }
-            return remain;
-        }
-        if(dp[i][j] != -1) return dp[i][j];
-        int same = 1e9;
-        int delete1 = 1e9;
-        int delete2 = 1e9;
-        if(s1[i] == s2[j]) {
-            same = helper(s1,s2,i-1,j-1,dp);
-        }
-        else{
-            delete1 = s1[i]+helper(s1,s2,i-1,j,dp);
-            delete2 = s2[j]+helper(s1,s2,i,j-1,dp);
-        }
-        return dp[i][j] = min({same,delete1,delete2});
-    }
+    
     int minimumDeleteSum(string s1, string s2) {
         int n = s1.size();
         int m = s2.size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return helper(s1,s2,n-1,m-1,dp);
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int j = 1;j<=m;j++) {
+            dp[0][j] = dp[0][j-1]+s2[j-1];
+        }
+        for(int i = 1;i<=n;i++) {
+            dp[i][0] = dp[i-1][0]+s1[i-1];
+        }
+        for(int i = 1;i<=n;i++) {
+            for(int j = 1;j<=m;j++) {
+                if(s1[i-1] == s2[j-1]) {
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = min(s1[i-1]+dp[i-1][j],s2[j-1]+dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
