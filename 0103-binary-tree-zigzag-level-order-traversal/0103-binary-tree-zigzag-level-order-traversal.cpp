c7f2bca867ba741945acef2de3fd053269920d6d
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2026-01-17 18:51:16 +0530

    Time: 0 ms (100%), Space: 15 MB (76.95%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -11,39 +11,40 @@
  */
 class Solution {
 public:
-    void helper(TreeNode* root, vector<vector<int>>&res) {
-        if(root == NULL) return;
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
         deque<TreeNode*>dq;
+        vector<vector<int>>ans;
+        if(root == nullptr) return ans;
         dq.push_back(root);
-        bool reverse = false;
+        int level = 0;
         while(!dq.empty()) {
-            vector<int>lvl;
-            int size = dq.size();
-           
-            for(int i = 0;i<size;i++) {
-                if(reverse == false) {
+            
+            if(level%2 == 0) {
+                int n = dq.size();
+                vector<int>lvl;
+                for(int i = 0;i<n;i++) {
                     TreeNode* node = dq.front();
                     dq.pop_front();
                     lvl.push_back(node->val);
                     if(node->left) dq.push_back(node->left);
                     if(node->right) dq.push_back(node->right);
                 }
-                else{
+                ans.push_back(lvl);
+            }
+            else{
+                int n = dq.size();
+                vector<int>lvl;
+                for(int i = 0;i<n;i++) {
                     TreeNode* node = dq.back();
                     dq.pop_back();
                     lvl.push_back(node->val);
                     if(node->right) dq.push_front(node->right);
                     if(node->left) dq.push_front(node->left);
                 }
+                ans.push_back(lvl);
             }
-            res.push_back(lvl);
-            reverse = !reverse;
+            level++;
         }
-    }
-    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        vector<vector<int>>res;
-        if(root == NULL) return res;
-        helper(root, res);
-        return res;
+        return ans;
     }
 };
