225ef3ee09119d7b07ce3f4caa1af3da0f5a4724
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2026-01-30 02:31:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.cpp
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -10,27 +10,68 @@
  * };
  */
 class Solution {
-    int maxDistance = 0;
 public:
-    int helper(TreeNode* root,int start) {
-        int depth = 0;
-        if(root == NULL) return depth;
-        int left = helper(root->left, start);
-        int right = helper(root->right, start);
-        if(root->val == start) {
-            maxDistance = max(left,right);
-            depth = -1;
-        }else if(left >=0 && right >= 0){
-            depth = max(left,right)+1;
-        }else {
-            int distance = abs(left) + abs(right);
-            maxDistance = max(maxDistance, distance);
-            depth = min(left,right)-1;
+    TreeNode* treeToGraph(TreeNode* root, int start, unordered_map<TreeNode*,TreeNode*>&mp) {
+        queue<TreeNode*>q;
+        TreeNode* target = NULL;
+        q.push(root);
+        while(!q.empty()) {
+            int n = q.size();
+            for(int i = 0;i<n;i++) {
+                TreeNode* node = q.front();
+                q.pop();
+                if(node->val == start){
+                    target = node;
+                } 
+                if(node->left) {
+                    q.push(node->left);
+                    mp[node->left] = node;
+                }
+                if(node->right) {
+                    q.push(node->right);
+                    mp[node->right] = node;
+                }
+            }
         }
-        return depth;
+        return target;
     }
     int amountOfTime(TreeNode* root, int start) {
-        helper(root, start);
-        return maxDistance;
+        //As we dont have connectiviy to upper node 
+        //so we will make a parent map that will connect every node with its paernt like in graphs 
+        // and at the same time we will find the target Node.
+        //We will create a map having key->childNode, value->parent Node;
+        unordered_map<TreeNode*, TreeNode*>mp;
+        TreeNode* targetNode = treeToGraph(root,start,mp);
+        queue<TreeNode*>q;
+
+        q.push(targetNode);
+        unordered_map<TreeNode*, bool>visited;
+        visited[targetNode] = true;
+        int time = 0;
+        while(!q.empty()) {
+            int n = q.size();
+            bool burnt = false;
+            for(int i = 0;i<n;i++) {
+                TreeNode* node = q.front();
+                q.pop();
+                if(node->left && !visited[node->left]) {
+                    q.push(node->left);
+                    visited[node->left] = true;
+                    burnt = true;
+                }
+                if(node->right && !visited[node->right]) {
+                    q.push(node->right);
+                    visited[node->right] = true;
+                    burnt = true;
+                }
+                if(mp[node] && !visited[mp[node]]) {
+                    q.push(mp[node]);
+                    visited[mp[node]] = true;
+                    burnt = true;
+                }
+                if(burnt)time++;
+            }
+        }
+        return time;
     }
 };
