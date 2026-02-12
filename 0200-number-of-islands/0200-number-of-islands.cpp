f2e642fdd3891fcf7881b611aaf5c7ca8dee012a
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2026-02-12 22:18:47 +0530

    Time: 33 ms (21.35%), Space: 24.6 MB (11.21%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -19,7 +19,23 @@ public:
         for(int i = 0;i<n;i++) {
             for(int j = 0;j<m;j++) {
                 if(grid[i][j] == '1' && vis[i][j] == 0) {
-                    dfs(grid,vis,n,m,i,j);
+                    queue<pair<int,int>>q;
+                    q.push({i,j});
+                    vis[i][j] = 1;
+                    while(!q.empty()) {
+                        auto row = q.front().first;
+                        auto col = q.front().second;
+                        q.pop();
+                        
+                        for(auto &dir : directions) {
+                            int new_i = row+dir[0];
+                            int new_j = col+dir[1];
+                            if(new_i>=0 && new_j>=0 && new_i<n && new_j<m && grid[new_i][new_j] == '1' && vis[new_i][new_j]== 0) {
+                                q.push({new_i,new_j});
+                                vis[new_i][new_j] =1;
+                            }
+                        }
+                    }
                     cnt++;
                 }
             }
