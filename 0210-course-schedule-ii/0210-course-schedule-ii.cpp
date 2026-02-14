29a68ba68d2928be8e3526c7a56927ce52fa1151
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2026-02-14 18:14:32 +0530

    Time: 0 ms (100%), Space: 18.7 MB (43.8%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,36 +1,40 @@
 class Solution {
 public:
+    bool dfs(int node,vector<vector<int>>&graph,vector<int>&vis,vector<int>&path,stack<int>&st) {
+        vis[node] = 1;
+        path[node] = 1;
+        for(auto adjNode : graph[node]) {
+            if(vis[adjNode] == 0) {
+                if(dfs(adjNode,graph,vis,path,st)){
+                    return true;
+                }
+                
+            }else if(path[adjNode] ==1) {
+                return true;
+            }
+        }
+        path[node] = 0;
+        st.push(node);
+        return false;
+    }
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>>graph(numCourses);
         for(auto &it: prerequisites) {
             graph[it[1]].push_back(it[0]);
         }
-        vector<int>indegree(numCourses,0);
+        vector<int>vis(numCourses,0);
+        vector<int>path(numCourses,0);
+        stack<int>st;
         for(int i = 0;i<numCourses;i++) {
-            for(auto &it:graph[i]) {
-                indegree[it]++;
-            }
-        }
-        queue<int>q;
-        for(int i = 0;i<numCourses;i++) {
-            if(indegree[i]==0) {
-                q.push(i);
+            if(vis[i] == 0) {
+                if(dfs(i,graph,vis,path,st)) return {};
             }
         }
         vector<int>ans;
-        while(!q.empty()) {
-            int node = q.front();
-            q.pop();
-            ans.push_back(node);
-            for(int adjNode : graph[node]) {
-                indegree[adjNode]--;
-                if(indegree[adjNode] == 0) {
-                    q.push(adjNode);
-                }
-            }
+        while(!st.empty()) {
+            ans.push_back(st.top());
+            st.pop();
         }
-        if(ans.size() == numCourses)
-            return ans;
-        return {};
+        return ans;
     }
 };
