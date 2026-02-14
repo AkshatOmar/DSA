904e1d17deff19170041414a2c231b578d6e14a6
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2026-02-14 15:11:05 +0530

    Time: 8 ms (37.02%), Space: 20.1 MB (36.21%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,33 +1,38 @@
 class Solution {
 public:
-    bool dfs(int node, int numCourses, vector<vector<int>>&graph,vector<int>&vis,vector<int>&path) {
-        vis[node] = 1;
-        path[node] = 1;
-        for(auto &adjNode: graph[node]) {
-            if(vis[adjNode]==0) { 
-                if(dfs(adjNode,numCourses,graph,vis,path)==true) return true;
-            }
-            else if(path[adjNode] == 1 && vis[adjNode] == 1) {
-                return true;
-            }
-            
-        }
-        path[node] = 0;
-        return false;
-    }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>>graph(numCourses);
-        for(int i = 0;i<prerequisites.size();i++) {
-            int u = prerequisites[i][1];
-            int v = prerequisites[i][0];
+        for(auto it : prerequisites) {
+            int u = it[1];
+            int v = it[0];
             graph[u].push_back(v);
         }
-        vector<int>vis(numCourses,0);
-        vector<int>path(numCourses,0);
+        queue<int>q;
+        vector<int>indegree(numCourses);
         for(int i = 0;i<numCourses;i++) {
-            if(vis[i] == 0) {
-                if(dfs(i,numCourses,graph,vis,path))return false;
+            for(auto it : graph[i]) {
+                indegree[it]++;
             }
+        }
+        for(int i = 0;i<indegree.size();i++) {
+            if(indegree[i] == 0) {
+                q.push(i);
+            }
+        }
+        vector<int>ans;
+        while(!q.empty()) {
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+            for(auto it:graph[node]) {
+                indegree[it]--;
+                if(indegree[it] == 0) {
+                    q.push(it);
+                }
+            }
+        }
+        if(ans.size()!= numCourses) {
+            return false;
         }
         return true;
     }
