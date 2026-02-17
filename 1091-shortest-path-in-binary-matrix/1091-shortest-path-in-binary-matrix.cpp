33de7b3cdc41e9f69c6dc952b5d03c2172056963
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2026-02-17 23:16:26 +0530

    Time: 27 ms (32.79%), Space: 23.7 MB (73.07%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -7,30 +7,30 @@ public:
         if(grid[0][0] != 0) {
             return -1;
         }
-        queue<pair<int,int>>q;
-        q.push({0,0});
-        int lvl = 0;
-        while(!q.empty()) {
-            int sz = q.size();
-            for(int i = 0;i<sz;i++) {
-                int x = q.front().first;
-                int y = q.front().second;
-                q.pop();
-                if(x == n-1 && y == m-1) {
-                    return lvl+1;
-                }
-                for(auto &dir : directions) {
-                    int new_x = x+dir[0];
-                    int new_y = y+dir[1];
-                    
-                    if(new_x >= 0 && new_x<n && new_y >= 0 && new_y<n && grid[new_x][new_y]==0) {
-                        q.push({new_x,new_y});
-                        grid[new_x][new_y]=1;
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
+        pq.push({0,{0,0}});
+        vector<vector<int>>dist(n,vector<int>(m,INT_MAX));
+        dist[0][0] = 0;
+        while(!pq.empty()) {
+            int dis = pq.top().first;
+            int i = pq.top().second.first;
+            int j = pq.top().second.second;
+            pq.pop();
+            for(auto &dir:directions) {
+                int new_i = i+dir[0];
+                int new_j = j+dir[1];
+                if(new_i>=0 && new_i<n && new_j>=0 && new_j<m && grid[new_i][new_j]==0) {
+                    if(dis + 1< dist[new_i][new_j]){
+                        dist[new_i][new_j] = dis+1;
+                        pq.push({dis+1,{new_i,new_j}});
+                        grid[new_i][new_j] = 1;
                     }
                 }
             }
-            lvl++;
         }
+        if(dist[n-1][m-1] == INT_MAX)
         return -1;
+
+        return dist[n-1][m-1]+1;
     }
 };
