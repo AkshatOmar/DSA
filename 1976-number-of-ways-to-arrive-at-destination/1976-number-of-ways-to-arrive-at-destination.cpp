a367af93c730adedb1eebf0391511df98bd5150a
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2026-02-19 18:23:04 +0530

    Time: 18 ms (14.93%), Space: 37.2 MB (46.1%) - LeetHub

--- 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
+++ 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
@@ -6,28 +6,33 @@ public:
             graph[it[0]].push_back({it[1],it[2]});
             graph[it[1]].push_back({it[0],it[2]});
         }
-        priority_queue<pair<long long,int>,vector<pair<long long,int>>,greater<pair<long long,int>>>pq;
-        pq.push({0,0});
+        long long mod = 1e9+7;
+        priority_queue<
+            pair<long long,int>,
+            vector<pair<long long,int>>,
+            greater<pair<long long,int>>
+        > q;
+
+        q.push({0,0});
         vector<long long>dist(n,LLONG_MAX);
+        vector<long long>ways(n,0);
         dist[0] = 0;
-        vector<int>ways(n,0);
         ways[0] = 1;
-        int mod =  (1e9+7);
-        while(!pq.empty()) {
-            int node = pq.top().second;
-            long long cost = pq.top().first;
-            pq.pop();
-            if(cost>dist[node])continue;
+        while(!q.empty()) {
+            long long cost = q.top().first;
+            int node = q.top().second;
+            q.pop();
+            if(cost>dist[node]) continue;
             for(auto it : graph[node]) {
                 int adj = it.first;
-                int wt = it.second;
-                if(wt+cost < dist[adj]) {
+                long long wt = it.second;
+                if(wt+cost<dist[adj]) {
                     dist[adj] = wt+cost;
                     ways[adj] = ways[node];
-                    pq.push({dist[adj],adj});
+                    q.push({dist[adj],adj});
                 }
-                else if(wt+cost == dist[adj]) {
-                    ways[adj] = (ways[adj]+ways[node])%mod;
+                else if(wt + cost == dist[adj]) {
+                    ways[adj] = (ways[adj] + ways[node])%mod;
                 }
             }
         }
