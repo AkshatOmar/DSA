5180a5111045437d6bfa4726ae5280f43c16d084
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2026-02-21 23:20:28 +0530

    Time: 15 ms (70.57%), Space: 23.8 MB (71.48%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,36 +1,35 @@
 class Solution {
 public:
-    vector<vector<int>>directions{{-1,0},{1,0},{0,-1},{0,1},{-1,-1},{1,1},{-1,1},{1,-1}};
+    vector<vector<int>>directions{{-1,0},{1,0},{0,-1},{0,1},{1,1},{-1,1},{1,-1},{-1,-1}};
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n = grid.size();
-        int m = grid[0].size();
-        if(grid[0][0] != 0) {
-            return -1;
-        }
-        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
-        pq.push({0,{0,0}});
-        vector<vector<int>>dist(n,vector<int>(m,INT_MAX));
-        dist[0][0] = 0;
-        while(!pq.empty()) {
-            int dis = pq.top().first;
-            int i = pq.top().second.first;
-            int j = pq.top().second.second;
-            pq.pop();
-            for(auto &dir:directions) {
-                int new_i = i+dir[0];
-                int new_j = j+dir[1];
-                if(new_i>=0 && new_i<n && new_j>=0 && new_j<m && grid[new_i][new_j]==0) {
-                    if(dis + 1< dist[new_i][new_j]){
-                        dist[new_i][new_j] = dis+1;
-                        pq.push({dis+1,{new_i,new_j}});
+        if(grid[0][0] == 1 || grid[n-1][n-1]==1) return -1;
+        queue<pair<int,int>>q;
+        //push i,j in queue
+        q.push({0,0});
+        //initialize sum for returning ansl
+        int sum = 0;
+        while(!q.empty()) {
+            int size = q.size();
+            for(int k =0;k<size;k++) {
+                int i = q.front().first;
+                int j = q.front().second;
+                q.pop();
+                
+                if(i == n-1 && j == n-1) {
+                    return sum+1;
+                }
+                for(auto &dir : directions) {
+                    int new_i = i + dir[0];
+                    int new_j = j + dir[1];
+                    if(new_i >=0 && new_i<n && new_j>=0 && new_j < n && grid[new_i][new_j] == 0) {
                         grid[new_i][new_j] = 1;
+                        q.push({new_i,new_j});
                     }
                 }
             }
+            sum++;
         }
-        if(dist[n-1][m-1] == INT_MAX)
         return -1;
-
-        return dist[n-1][m-1]+1;
     }
 };
