e80c4c743c3f4682e7a9c6276474b293a693a0d2
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2026-02-22 23:41:55 +0530

    Time: 32 ms (21.69%), Space: 48.3 MB (29.18%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -34,14 +34,14 @@ class DSU {
 
 class Solution {
 public:
-    void dfs(int node, vector<vector<int>>&adj, vector<int>&vis) {
-        vis[node] = 1;
-        for(auto it : adj[node]) {
-            if(!vis[it]) {
-                dfs(it,adj,vis);
-            }
-        }
-    }
+    // void dfs(int node, vector<vector<int>>&adj, vector<int>&vis) {
+    //     vis[node] = 1;
+    //     for(auto it : adj[node]) {
+    //         if(!vis[it]) {
+    //             dfs(it,adj,vis);
+    //         }
+    //     }
+    // }
     int makeConnected(int n, vector<vector<int>>& connections) {
         int avlConnections = connections.size();
         if(n-1 > avlConnections) return -1;
@@ -55,22 +55,26 @@ public:
             }
             else {
                 ds.unionFind(a,b);
-                
             }
         }
-        vector<vector<int>>adj(n);
-        for(auto it : connections) {
-            adj[it[0]].push_back(it[1]);
-            adj[it[1]].push_back(it[0]);
-        }
-        int components = 0;
-        vector<int>vis(n,0);
+        //int components = 0;
+        // vector<vector<int>>adj(n);
+        // for(auto it : connections) {
+        //     adj[it[0]].push_back(it[1]);
+        //     adj[it[1]].push_back(it[0]);
+        // }
+        // vector<int>vis(n,0);
+        // for(int i = 0;i<n;i++) {
+        //     if(!vis[i]) {   
+        //         dfs(i,adj,vis);
+        //         components++;
+        //     }
+        // }
+        unordered_set<int>st;
         for(int i = 0;i<n;i++) {
-            if(!vis[i]) {   
-                dfs(i,adj,vis);
-                components++;
-            }
+            st.insert(ds.find(i));
         }
+        int components = st.size();
         if(extracnt >= components-1) return components-1;
         return -1;
         
