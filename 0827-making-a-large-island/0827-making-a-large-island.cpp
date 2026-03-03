a5654c97444196b7abb2d9512a6510dafd051a93
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2026-03-03 08:37:49 +0530

    Time: 172 ms (87.14%), Space: 109.7 MB (78.64%) - LeetHub

--- 0827-making-a-large-island/0827-making-a-large-island.cpp
+++ 0827-making-a-large-island/0827-making-a-large-island.cpp
@@ -1,126 +1,94 @@
 class DSU {
-    vector<int>rank,parent,size;
-    public:
+    vector<int>size,parent;
+public : 
     DSU(int n) {
-        rank.resize(n,0);
-        parent.resize(n);
         size.resize(n,1);
-        for(int i = 0;i<n;i++) {
+        parent.resize(n);
+        for(int i =0;i<n;i++)  {
             parent[i] = i;
         }
     }
-
     int find(int i) {
         if(parent[i] == i) {
             return i;
         }
         return parent[i] = find(parent[i]);
-  
     }
-
-    void unionByrank(int x,int y) {
-        int new_x = find(x);
-        int new_y = find(y);
-        if(new_x == new_y) 
-            return;
-        
-        if(rank[new_x] >rank[new_y]) {
-            parent[new_y] = new_x;
-        }
-
-        else if(rank[new_x] <rank[new_y]) {
-            parent[new_x] = new_y;
-        }
-
-        else {
-            parent[new_x] = new_y;
-            rank[new_y]++;
-        }
-    }
-    void unionBySize(int x,int y) {
-        int new_x = find(x);
-        int new_y = find(y);
-
-        if(size[new_x] < size[new_y]) {
-            parent[new_x] = new_y;
-            size[new_y] += size[new_x];
+    void unionBySize(int u, int v) {
+        int new_u = find(u);
+        int new_v = find(v);
+        if(new_u == new_v) return;
+        if(size[new_u] > size[new_v]) {
+            parent[new_v] = new_u;
+            size[new_u] += size[new_v];
         }
         else {
-            parent[new_y] = new_x;
-            size[new_x] += size[new_y];
+            parent[new_u] = new_v;
+            size[new_v] += size[new_u];
         }
     }
-    int getSize(int x) {
-        int root = find(x);
-        return size[root];
+    int getSize(int node) {
+        return size[find(node)];
     }
 };
 class Solution {
-private: 
-    bool isValid(int i,int j,int n) {
-        return (i >= 0 && j>=0 && j<n && i<n);
+private:
+    vector<vector<int>>directions{{-1,0},{1,0},{0,-1},{0,1}};
+    bool isValid(int i, int j ,int n) {
+        return (i>=0 && i<n && j >= 0 && j<n);
     }
-public:
-    vector<vector<int>>directions{{1,0},{-1,0},{0,-1},{0,1}};
-    int largestIsland(vector<vector<int>>& grid) {
-        int n = grid.size();
-        DSU ds(n*n);
+    void connectGrid(vector<vector<int>>&grid, int n,DSU &ds) {
+        
         for(int i = 0;i<n;i++) {
             for(int j = 0;j<n;j++) {
-                if(grid[i][j] == 0)continue;
-                int node = i*n + j;
+
+                if(grid[i][j] == 0) continue;
 
                 for(auto &dir : directions) {
                     int new_i = i + dir[0];
                     int new_j = j + dir[1];
-                    int adjNode = new_i * n + new_j;
-
                     if(isValid(new_i,new_j,n)) {
-
                         if(grid[new_i][new_j] == 1) {
-
-                            //Connected the islands
-                            if(ds.find(node) != ds.find(adjNode)) {
-                                ds.unionBySize(node,adjNode);
-                            }
-                            
+                            int adjNode = new_i * n + new_j;
+                            int node = i*n + j;
+                            ds.unionBySize(node,adjNode);
                         }
-
                     }
-
                 }
 
             }
         }
+    }
+public:
+    int largestIsland(vector<vector<int>>& grid) {
+        int n = grid.size();
+        DSU ds(n*n);
+        connectGrid(grid,n,ds);
         int ans = 0;
         for(int i = 0;i<n;i++) {
             for(int j = 0;j<n;j++) {
-                if(grid[i][j] == 1) continue;
+                if(grid[i][j] == 1)continue;
                 set<int>components;
-
-                for(auto dir : directions) {
+                for(auto &dir : directions) {
                     int new_i = i + dir[0];
                     int new_j = j + dir[1];
-                    int adjNode = new_i * n + new_j;
-
                     if(isValid(new_i,new_j,n)) {
                         if(grid[new_i][new_j] == 1) {
+                            int adjNode = new_i * n + new_j;
                             components.insert(ds.find(adjNode));
                         }
                     }
-
                 }
                 int totalSize = 0;
                 for(auto it : components) {
                     totalSize += ds.getSize(it);
                 }
-                ans = max(ans,totalSize+1);
+                ans = max(totalSize+1,ans);
             }
-            
         }
-        for(int i = 0;i<n*n;i++) {
-                ans = max(ans,ds.getSize(ds.find(i)));
-            }
+        for(int i =0;i<n*n;i++) {
+            ans = max(ds.getSize(ds.find(i)), ans);
+        }
         return ans;
     }
 };
