397e9a7de31f2692a1037d82b522c884d2622f5f
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2026-03-06 23:56:49 +0530

    Time: 2 ms (90.46%), Space: 14.5 MB (36.97%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
@@ -1,41 +1,69 @@
+class DSU {
+    vector<int>rank, parent;
+    public:
+    DSU(int n) {
+        rank.resize(n,0);
+        parent.resize(n);
+        for(int i = 0;i<n;i++) {
+            parent[i] = i;
+        }
+    }
+    int find(int i) {
+        if(parent[i] == i){
+            return i;
+        }
+        return parent[i] = find(parent[i]);
+    }
+    void unite(int u,int v) {
+        int ul_u = find(u);
+        int ul_v = find(v);
+        if(ul_u == ul_v) return;
+        if(rank[ul_v] >rank[ul_u]) {
+            parent[ul_u] = parent[ul_v];
+        }
+        else if(rank[ul_v] < rank[ul_u]) {
+            parent[ul_v] = parent[ul_u];
+        }
+        else {
+            parent[ul_v] = ul_u;
+            rank[ul_u]++;
+        }
+    }
+};
 class Solution {
-private: 
-vector<vector<int>>directions{{-1,0},{1,0},{0,-1},{0,1}};
-bool isValid(int new_i, int new_j, int n) {
-    return new_i >=0 && new_i < n && new_j >= 0 && new_j < n;
-}
+private:
+    vector<vector<int>>directions{{1,0},{-1,0},{0,1},{0,-1}};
+    bool isValid(int i, int j, int n) {
+        return (i>=0 && j<n && i<n && j>=0);
+    }
 public:
-    int swimInWater(vector<vector<int>>& grid) {
+    int swimInWater(vector<vector<int>>& grid) { 
         int n = grid.size();
+        DSU ds(n*n);
+        vector<tuple<int,int,int>>cells;
+        for(int i = 0;i<n;i++) {
+            for(int j = 0;j<n;j++) {
+                cells.push_back({grid[i][j],i,j});
+            }
+        }
+        sort(cells.begin(),cells.end());
         vector<vector<int>>vis(n,vector<int>(n,0));
-        //pq<int,vector,greater;-> 
-
-        priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
-        //{t,{i,j}};
-        pq.push({grid[0][0],{0,0}});
-        vis[0][0] = 1;
-        while(!pq.empty()) {
-            int t = pq.top().first;
-            int i = pq.top().second.first;
-            int j = pq.top().second.second;
-            pq.pop();
-    
-            if(i == n-1 && j == n-1) return t;
-
+        for(auto &[h,i,j] : cells) {
+            vis[i][j] = 1;
             for(auto &dir : directions) {
                 int new_i = i + dir[0];
                 int new_j = j + dir[1];
 
-                if(isValid(new_i,new_j,n)) {
-                    if(vis[new_i][new_j] == 0) {
-                        pq.push({max(t,grid[new_i][new_j]),{new_i,new_j}});
-                        vis[new_i][new_j] = 1;
-                    }
-                    
+                if(isValid(new_i,new_j,n) && vis[new_i][new_j] == 1) {
+                    ds.unite(i*n+j,new_i*n+new_j);
                 }
 
+            }
+            if(ds.find(0) == ds.find(n*n-1)){
+                return h;
             }
         }
         return -1;
     }
+
 };
