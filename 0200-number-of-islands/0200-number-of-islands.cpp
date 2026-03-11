54e4f6fad6cda81595fbef7e057bc9ce84962796
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2026-03-12 00:20:45 +0530

    Time: 29 ms (42.22%), Space: 17.6 MB (42.58%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     vector<vector<int>>directions{{-1,0},{1,0},{0,-1},{0,1}};
-    void dfs(vector<vector<char>>&grid,vector<vector<int>>&vis,int n ,int m,int i,int j) { 
+    void dfs(vector<vector<char>>&grid, vector<vector<int>>&vis,int n , int m, int i, int j) {
         vis[i][j] = 1;
         for(auto &dir : directions) {
             int new_i = i+dir[0];
             int new_j = j+dir[1];
-            if(new_i>=0 && new_j>=0 && new_i<n && new_j<m && grid[new_i][new_j] == '1' && vis[new_i][new_j]== 0) {
-                dfs(grid,vis,n,m,new_i,new_j);
+            if(new_i >= 0 && new_i <n && new_j >= 0 && new_j<m) {
+                if(grid[new_i][new_j] == '1' && vis[new_i][new_j] == 0) {
+                    dfs(grid,vis,n,m,new_i,new_j);
+                }
             }
         }
     }
@@ -15,31 +17,17 @@ public:
         int n = grid.size();
         int m = grid[0].size();
         vector<vector<int>>vis(n,vector<int>(m,0));
-        int cnt = 0;
+        int cnt  =0;
         for(int i = 0;i<n;i++) {
             for(int j = 0;j<m;j++) {
                 if(grid[i][j] == '1' && vis[i][j] == 0) {
-                    queue<pair<int,int>>q;
-                    q.push({i,j});
-                    vis[i][j] = 1;
-                    while(!q.empty()) {
-                        auto row = q.front().first;
-                        auto col = q.front().second;
-                        q.pop();
-                        
-                        for(auto &dir : directions) {
-                            int new_i = row+dir[0];
-                            int new_j = col+dir[1];
-                            if(new_i>=0 && new_j>=0 && new_i<n && new_j<m && grid[new_i][new_j] == '1' && vis[new_i][new_j]== 0) {
-                                q.push({new_i,new_j});
-                                vis[new_i][new_j] =1;
-                            }
-                        }
-                    }
                     cnt++;
+                    dfs(grid, vis,n,m,i,j);
                 }
             }
+                
         }
+        
         return cnt;
     }
 };
