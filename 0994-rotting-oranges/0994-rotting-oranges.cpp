670ca1566f21fe063b1dca51cde8a946061249a6
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2026-03-12 13:59:37 +0530

    Time: 0 ms (100%), Space: 17.2 MB (30.53%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,44 +1,51 @@
 class Solution {
 public:
-    vector<vector<int>>direction{{-1,0},{1,0},{0,-1},{0,1}};
-    bool isSafe(int i, int j,int n,int m, vector<vector<int>>&grid) {
-        return (i>=0 && i<n && j>=0 && j<m && grid[i][j] == 1);
+    vector<vector<int>>directions{{-1,0},{1,0},{0,1},{0,-1}};
+    bool isValid(int i, int j, int n, int m) {
+        return (i >= 0 && j >= 0 && i<n && j<m);
     }
     int orangesRotting(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
+        int fresh = 0;
         queue<pair<int,int>>q;
-        int freshcnt = 0;
         for(int i = 0;i<n;i++) {
             for(int j = 0;j<m;j++) {
-                if(grid[i][j] == 1) {
-                    freshcnt++;
-                }else if(grid[i][j] == 2) {
+                if(grid[i][j] == 2) {
                     q.push({i,j});
+                }else if(grid[i][j] == 1) {
+                    fresh++;
                 }
             }
         }
-        if(freshcnt == 0) return 0;
-        int minute = 0;
+        int ans = 0;
+        if(fresh == 0) return ans;
         while(!q.empty()) {
             int size = q.size();
-            for(int r = 0;r<size;r++) {
-                auto it = q.front();
+            for(int k =0 ;k<size;k++) {
+                int i = q.front().first;
+                int j = q.front().second;
                 q.pop();
-                int i = it.first;
-                int j = it.second;
-                for(auto &dir : direction) {
+
+                for(auto &dir : directions) {
                     int new_i = i+dir[0];
-                    int new_j = j+dir[1];
-                    if(isSafe(new_i,new_j,n,m,grid)) {
-                        grid[new_i][new_j] = 2;
-                        q.push({new_i,new_j});
-                        freshcnt--;
+                    int new_j = j + dir[1];
+                    
+                    if(isValid(new_i,new_j,n,m)) {
+                        if(grid[new_i][new_j] == 1) {
+                            
+                            grid[new_i][new_j] = 2;
+                            q.push({new_i, new_j});
+                            fresh--;
+                        }
                     }
+                    
                 }
+                
             }
-            minute++;
+            ans++;
         }
-        return freshcnt == 0 ? minute-1 : -1;
+        
+        return fresh == 0 ? ans-1 : -1;
     }
 };
