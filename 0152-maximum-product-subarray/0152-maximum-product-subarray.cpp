75092ff4baebbdd6752279257e2db92161427d8f
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2026-03-14 13:02:12 +0530

    Time: 0 ms (100%), Space: 17.7 MB (59.33%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -2,15 +2,31 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums) {
         int n = nums.size();
-        int ans = nums[0];
-        int currMax=nums[0];
-        int currMin = nums[0];
-        for(int i = 1;i<n;i++) {
-            if(nums[i]<0) swap(currMax,currMin);
-            currMax = max(nums[i],nums[i]*currMax);
-            currMin = min(nums[i],nums[i]*currMin);
-            ans = max(currMax,ans);
+        int maxProd = INT_MIN;
+        int prod = 1;
+        
+        for(int i = 0;i<n;i++) {
+            prod = nums[i]*prod;
+            maxProd = max(prod,maxProd);
+            if(prod == 0) {
+                prod = 1;
+            }
+            if(nums[i] > maxProd) {
+                maxProd = nums[i];
+            }
         }
-        return ans;
+        int maxLeft = INT_MIN;
+        int leftProd = 1;
+        for(int i = n-1;i>=0;i--) {
+            leftProd = nums[i]*leftProd;
+            maxLeft = max(leftProd,maxLeft);
+            if(leftProd == 0) {
+                leftProd = 1;
+            }
+            if(nums[i] > maxLeft) {
+                maxLeft = nums[i];
+            }
+        }
+        return max(maxProd, maxLeft);
     }
 };
