cfb7752ec62261c4e8334640fe6887dcbe0e3781
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2026-03-14 13:17:59 +0530

    Time: 0 ms (100%), Space: 17.8 MB (59.33%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -2,27 +2,22 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums) {
         int n = nums.size();
-        int maxProd = INT_MIN;
-        int prod = 1;
-        
+        int pref = 1;
+        int suff = 1;
+        int maxPref = INT_MIN;
+        int maxSuff = INT_MIN;
         for(int i = 0;i<n;i++) {
-            prod = nums[i]*prod;
-            maxProd = max(prod,maxProd);
-            if(prod == 0) {
-                prod = 1;
+            pref = pref*nums[i];
+            suff  = suff* nums[n-1-i];
+            maxPref = max(maxPref,pref);
+            maxSuff = max(maxSuff,suff);
+            if(pref == 0){
+                pref = 1;
             }
-            
-        }
-        int maxLeft = INT_MIN;
-        int leftProd = 1;
-        for(int i = n-1;i>=0;i--) {
-            leftProd = nums[i]*leftProd;
-            maxLeft = max(leftProd,maxLeft);
-            if(leftProd == 0) {
-                leftProd = 1;
+            if(suff == 0) {
+                suff = 1;
             }
-           
         }
-        return max(maxProd, maxLeft);
+        return max(maxPref,maxSuff);
     }
 };
