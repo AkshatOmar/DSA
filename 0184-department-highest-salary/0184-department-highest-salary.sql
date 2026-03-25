d138fdc4300a4fe5cff3041ae9eb010c92bed229
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2026-03-25 14:27:01 +0530

    Time: 961 ms (28.9%), Space: 0B (100%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,15 +1,25 @@
 # Write your MySQL query statement below
-WITH employee_details AS 
-(
-    SELECT e.name AS Employee, e.salary AS Salary, d.name AS Department, 
-    DENSE_RANK() OVER(PARTITION BY d.name ORDER BY salary DESC) AS rnk
-    FROM Employee e 
-    LEFT JOIN Department d 
-    ON e.departmentId = d.id
+-- WITH employee_details AS 
+-- (
+--     SELECT e.name AS Employee, e.salary AS Salary, d.name AS Department, 
+--     DENSE_RANK() OVER(PARTITION BY d.name ORDER BY salary DESC) AS rnk
+--     FROM Employee e 
+--     LEFT JOIN Department d 
+--     ON e.departmentId = d.id
     
-)
+-- )
 
-SELECT Department, Employee, Salary 
-FROM employee_details
-WHERE rnk = 1 
-;
+-- SELECT Department, Employee, Salary 
+-- FROM employee_details
+-- WHERE rnk = 1 
+-- ;
+SELECT d.name AS Department,e.name AS Employee, e.salary AS Salary
+FROM Employee e
+JOIN Department d
+ON e.departmentId = d.id
+WHERE 
+    e.salary = (
+                SELECT MAX(salary)
+                FROM Employee
+                WHERE  departmentId = e.departmentId
+                )
