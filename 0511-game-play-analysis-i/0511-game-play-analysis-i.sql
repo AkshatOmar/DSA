0ccd525cddcd52b8268614ae60a35b28d6bf6d25
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2026-03-29 00:43:45 +0530

    Time: 516 ms (30.26%), Space: 0B (100%) - LeetHub

--- 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
+++ 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
@@ -1,10 +1,5 @@
 # Write your MySQL query statement below
-WITH CTE AS
-(
-    SELECT *, 
-    DENSE_RANK() OVER(PARTITION BY player_id ORDER BY event_date) AS rnk
-    FROM Activity
-)
-SELECT player_id, event_date AS first_login
-FROM CTE 
-WHERE rnk = 1 ;
+
+SELECT player_id, MIN(event_date) AS first_login
+FROM Activity 
+GROUP BY player_id;
