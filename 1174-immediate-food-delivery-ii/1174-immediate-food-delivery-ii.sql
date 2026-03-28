9cd53080557a902c3c691b61ee96e2ebdd61cc8a
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2026-03-28 12:38:33 +0530

    Time: 605 ms (56.96%), Space: 0B (100%) - LeetHub

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -5,21 +5,13 @@ WITH delivery_details AS
     ROW_NUMBER() OVER(PARTITION BY customer_id ORDER BY order_date)
     AS row_num
     FROM Delivery
-),
- deli AS
-(
-    SELECT *
-    FROM delivery_details
-    WHERE  
-    row_num = 1
-    GROUP BY customer_id
 )
 
 SELECT ROUND(
             SUM(CASE WHEN order_date = customer_pref_delivery_date THEN 1 
             ELSE 0 END) *100.0
             /
-            (SELECT COUNT(*) FROM deli) 
+             COUNT(*)
         ,2) AS immediate_percentage
-
-FROM deli;
+FROM delivery_details
+WHERE row_num  = 1;
