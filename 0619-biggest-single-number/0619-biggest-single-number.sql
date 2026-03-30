fc9c0f66028ac56da532c06545205d1070ed5788
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2026-03-30 14:09:10 +0530

    Time: 2067 ms (5.01%), Space: 0B (100%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,12 +1,9 @@
 # Write your MySQL query statement below
-WITH CTE AS 
-(
-    SELECT num, COUNT(num) AS cnt
-    FROM MyNumbers
-    GROUP BY num
-    ORDER BY num DESC
-)
+
 SELECT MAX(num) AS num
-FROM CTE 
-WHERE cnt = 1
+FROM MyNumbers
+WHERE num IN (SELECT MAX(num)
+                FROM MyNumbers
+                GROUP BY num
+                HAVING COUNT(*) = 1)
 ;
