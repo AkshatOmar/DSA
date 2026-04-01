0efd91bb8a4ab31388067e61c658ef34dcd4385a
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2026-04-02 00:52:34 +0530

    Time: 146 ms (5.62%), Space: 183.9 MB (19.14%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,72 +1,39 @@
-struct Node {
-    int key;
-    int value;
-    Node *next;
-    Node*prev;
-    Node(int k, int v){
-        key = k;
-        value = v;
-        next = NULL;
-        prev = NULL;
-    }
-};
 class LRUCache {
-  private:
-    int capacity;
-    unordered_map<int, Node*>cacheMap;
-    Node *head;
-    Node*tail;
-    void add(Node *node) {
-        Node *nextNode = head->next;
-        head->next = node;
-        node->prev = head;
-        node->next = nextNode;
-        nextNode->prev = node;
-    }
-    void remove(Node *node) {
-        Node *prevNode = node->prev;
-        Node *nextNode = node->next;
-        prevNode->next = nextNode;
-        nextNode->prev = prevNode;
-    }
-  public:
-    // Constructor for initializing the cache capacity with the given value.
-    
+public:
+    list<int>dll;
+    map<int,pair<list<int>:: iterator, int>>mp;
+    int n;
     LRUCache(int capacity) {
-        // code here
-        this->capacity = capacity;
-        head = new Node(-1, -1);
-        tail = new Node(-1, -1);
-        head->next = tail;
-        tail->prev = head;
+        n = capacity;
     }
-
-    // Function to return value corresponding to the key.
+    void makeRecentUsed(int key) {
+        dll.erase(mp[key].first);
+        dll.push_front(key);
+        mp[key].first = dll.begin();
+    }
     int get(int key) {
-        // your code here
-        if(cacheMap.find(key) == cacheMap.end()) return -1;
-        Node *node =cacheMap[key];
-        remove(node);
-        add(node);
-        return node->value;
-    }
-
-    // Function for storing key-value pair.
-    void put(int key, int value) {
-        // your code here
-        if(cacheMap.find(key) != cacheMap.end()) {
-            Node *oldNode = cacheMap[key];
-            remove(oldNode);
-            delete oldNode;
+        if(mp.find(key) == mp.end()) {
+            return -1;
         }
-        Node *node = new Node(key, value);
-        cacheMap[key] = node;
-        add(node);
-        if(cacheMap.size() > capacity) {
-            Node *nodeToDelete = tail->prev;
-            remove(nodeToDelete);
-            cacheMap.erase(nodeToDelete->key);
-            delete nodeToDelete;
+        makeRecentUsed(key);
+        return mp[key].second;
+    }
+    
+    void put(int key, int value) {
+        if(mp.find(key) != mp.end()) {
+            mp[key].second = value;
+            makeRecentUsed(key);
+        }
+        else {
+            dll.push_front(key);
+            mp[key] = {dll.begin(),value};
+            n--;
+        }
+        if(n < 0) {
+            int key_to_del = dll.back();
+            mp.erase(key_to_del);
+            dll.pop_back();
+            n++;
         }
     }
 };
