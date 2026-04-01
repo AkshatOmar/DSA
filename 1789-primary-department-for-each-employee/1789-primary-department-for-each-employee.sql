db5174f9dc3921cb748cb6c642c37d411be6305c
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2026-04-02 04:02:34 +0530

    Time: 531 ms (53.95%), Space: 0B (100%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -1,12 +1,11 @@
 # Write your MySQL query statement below
+WITH CTE AS
+(
+    SELECT employee_id, department_id, primary_flag,
+    COUNT(*) OVER (PARTITION BY employee_id) AS cnt
+    FROM Employee
+    
+)
 SELECT employee_id, department_id
-FROM Employee
-WHERE primary_flag = 'Y'
-
-UNION
-
-SELECT employee_id, department_id
-FROM Employee 
-GROUP BY employee_id 
-HAVING COUNT(employee_id) = 1 
-    ;
+FROM CTE 
+WHERE cnt = 1 OR primary_flag = 'Y';
