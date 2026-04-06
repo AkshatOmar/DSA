3eb054067c7b5b7ddb5937ae73a58c7900ee6f16
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2026-04-07 00:32:34 +0530

    Time: 532 ms (36.12%), Space: 0B (100%) - LeetHub

---
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -0,0 +1,18 @@
+# Write your MySQL query statement below
+WITH CTE AS
+(
+    SELECT * , 
+    ROW_NUMBER() OVER(PARTITION BY product_id ORDER BY change_date DESC) AS row_num
+    FROM Products
+    WHERE change_date <= '2019-08-16'
+)
+SELECT product_id, new_price AS price
+FROM CTE
+WHERE  row_num = 1
+
+UNION
+
+SELECT product_id, 10 AS price
+FROM Products
+WHERE product_id NOT IN (SELECT DISTINCT product_id FROM CTE)
+;
