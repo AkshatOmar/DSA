d057353ac28ceb835536c3476ce44f62cb02b125
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2026-04-07 03:55:03 +0530

    Time: 0 ms (100%), Space: 12.3 MB (65.91%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -2,11 +2,12 @@ class Solution {
 public:
     int hIndex(vector<int>& citations) {
         sort(citations.begin(),citations.end(),greater<int>());
-        int n=citations.size();
-        int index=0;
-        while(index<n&&citations[index]>index){
-            index+=1;
+        int cnt = 0;
+        int n = citations.size();
+
+        for(int i = 0;i<n;i++) {
+            if(citations[i] >= i+1) cnt++;
         }
-        return index;
+        return cnt;
     }
 };
