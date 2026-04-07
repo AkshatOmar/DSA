a9dce21c1c1f1ad6e0d7c7ae3dcc700b7db915a1
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2026-04-07 11:56:42 +0530

    Time: 0 ms (100%), Space: 43.9 MB (5.21%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -8,10 +8,11 @@ public:
 
         for(int i = 1;i<n;i++) {
             pref[i] = pref[i-1] * nums[i-1];
+            suff[n-i-1] = suff[n-i] * nums[n-i]; 
         }
-        for(int i = n-2;i>=0;i--) {
-            suff[i] = suff[i+1]* nums[i+1];
-        }
+        // for(int i = n-2;i>=0;i--) {
+        //     suff[i] = suff[i+1]* nums[i+1];
+        // }
         for(int i = 0;i<n;i++) {
             ans.push_back(pref[i]*suff[i]);
         }
