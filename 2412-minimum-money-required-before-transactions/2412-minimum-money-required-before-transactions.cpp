ee78f24a534f7d82aec6a57d1381a8b448824563
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2026-04-08 14:07:42 +0530

    Time: 11 ms (40.86%), Space: 122.8 MB (24.73%) - LeetHub

--- 2412-minimum-money-required-before-transactions/2412-minimum-money-required-before-transactions.cpp
+++ 2412-minimum-money-required-before-transactions/2412-minimum-money-required-before-transactions.cpp
@@ -5,7 +5,7 @@ public:
         vector<pair<int,int>>profit;
         vector<pair<int,int>>loss;
 
-        for(auto it : transactions) {
+        for(auto &it : transactions) {
             long long cost = it[0];
             long long cashback = it[1];
             if(cashback >= cost) {
@@ -17,25 +17,19 @@ public:
         }
         sort(profit.begin(),profit.end(), greater<pair<int,int>>());
         sort(loss.begin(), loss.end());
-        for(auto it : profit) {
-            cout<<it.first<<" "<<it.second<<" "<<it.second-it.first<<endl;
-        }
-        cout<<endl;
-        for(auto it : loss) {
-            cout<<it.second<<" "<<it.first<<" "<< it.second-it.first <<endl;
-        }
-        int ans = INT_MIN;
-        int curr = 0;
+        
+        long long ans = INT_MIN;
+        long long curr = 0;
         for(auto &it : loss) {
-            int cost = it.second;
-            int cashback = it.first;
+            long long cost = it.second;
+            long long cashback = it.first;
             curr += cost;
             ans = max(ans, curr);
             curr -= cashback;
         }
         for(auto &it : profit) {
-            int cost = it.first;
-            int cashback = it.second;
+            long long cost = it.first;
+            long long cashback = it.second;
             curr+= cost;
             ans = max(ans, curr);
             curr -= cashback;
