d310e193f12777ba4f15a201660c92a4adbc08a3
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2026-04-22 10:33:32 +0530

    Time: 3 ms (66.28%), Space: 10.9 MB (69.93%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -2,30 +2,20 @@ class Solution {
 public:
     int characterReplacement(string s, int k) {
         int n = s.size();
-        int i = 0, j = 0;
+        vector<int>freq(26,0);
+        int i = 0,j = 0;
         int maxFreq = 0;
-        unordered_map<char, int>mp;
         int maxLen = 0;
-        int replace = 0;
+
         while(j<n) {
-            mp[s[j]]++;
-            maxFreq = max(maxFreq, mp[s[j]]);
-            replace = j-i+1 - maxFreq;
-            //shrink the window;
-            while(replace > k) {
-                mp[s[i]]--;
-                int currMax = INT_MIN;
-                for(auto &it : mp) {
-                    currMax = max(currMax, it.second);
-                }
-                //updated maxFrequency.
-                maxFreq = currMax;
+            freq[s[j] -'A']++;
+            maxFreq = max(maxFreq,freq[s[j] - 'A']);
+            
+            while(j-i+1 - maxFreq > k) {
+                freq[s[i]-'A']--;
                 i++;
-                replace = j-i+1 - maxFreq;
             }
-            if(replace <= k) {
-                maxLen = max(maxLen, j-i+1);
-            }
+            maxLen = max(maxLen,j-i+1);
             j++;
         }
         return maxLen;
