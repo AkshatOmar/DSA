b2442807dabdf64ad94ba0560bef9f074e178ede
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2026-05-02 15:20:26 +0530

    Time: 0 ms (100%), Space: 32.7 MB (62.19%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-    int func(vector<int>&nums, int goal) {
-        int n = nums.size();
+    int subarray(vector<int>&nums,int goal) {
         if(goal < 0) return 0;
+        int n = nums.size();
         int i = 0, j = 0;
-        long long pref = 0;
+        int sum = 0;
         int cnt = 0;
         while(j<n) {
-            pref += nums[j];
-            while(pref > goal) {
-                pref -= nums[i];
+            if(nums[j] == 1) {
+                sum++;
+            }
+            while(sum > goal) {
+                if(nums[i] == 1) sum--;
                 i++;
-
             }
             cnt += (j-i+1);
             j++;
@@ -19,7 +20,6 @@ public:
         return cnt;
     }
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-        int n = nums.size();
-        return func(nums,goal) - func(nums,goal - 1);
+        return subarray(nums,goal) - subarray(nums,goal-1);
     }
 };
