c26123cf78b7e7a78a68bb2b38469bb7543cfe62
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2026-05-27 02:52:38 +0530

    Time: 1368 ms (39.16%), Space: 0B (100%) - LeetHub

--- 1341-movie-rating/1341-movie-rating.sql
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -1,21 +1,29 @@
 # Write your MySQL query statement below
-(SELECT u.name as results
-FROM MovieRating mr
- JOIN Users u
-    ON u.user_id = mr.user_id
+WITH CTE AS (
+    SELECT u.user_id,u.name,COUNT(m.movie_id) AS cnt
+    FROM MovieRating AS m
+    LEFT JOIN 
+    users u 
+    ON u.user_id = m.user_id
+    GROUP BY m.user_id,u.name
+    ORDER BY cnt DESC,u.name ASC
+    LIMIT 1
+),
 
-GROUP BY mr.user_id
-ORDER BY COUNT(*) DESC, u.name ASC
-LIMIT 1)
+ CTE2 AS (
+    SELECT m.title, AVG(rating) as avg_rating
+    FROM Movies m
+    RIGHT JOIN MovieRating mr
+    ON m.movie_id = mr.movie_id
+    WHERE mr.created_at LIKE '2020-02%'
+    GROUP BY mr.movie_id,m.title
+    ORDER BY avg_rating DESC, m.title ASC
+    LIMIT 1
+)
+SELECT name  AS results
+FROM CTE 
+UNION ALL
+SELECT title FROM CTE2;
 
-UNION ALL 
 
-(SELECT m.title AS results
-FROM Movies m
-JOIN MovieRating mr
-ON mr.movie_id = m.movie_id
-WHERE mr.created_at BETWEEN '2020-02-01' AND '2020-02-29'
-GROUP BY mr.movie_id
-ORDER BY AVG(rating) DESC, m.title ASC
-LIMIT 1)
-;
+
