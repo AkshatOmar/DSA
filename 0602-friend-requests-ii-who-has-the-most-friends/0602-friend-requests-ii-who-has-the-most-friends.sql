e674a28a12082b089211b354f55bb92cc4c22523
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2026-05-30 01:38:57 +0530

    Time: 319 ms (50.16%), Space: 0B (100%) - LeetHub

--- 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -1,14 +1,18 @@
 # Write your MySQL query statement below
 WITH CTE AS (
-    SELECT requester_id AS id
-    FROM RequestAccepted
+    SELECT requester_id 
+    FROM RequestAccepted 
     UNION ALL
-    SELECT accepter_id AS id
+    SELECT accepter_id
     FROM RequestAccepted
 
+), 
+CTE2 AS (
+    SELECT requester_id AS id, COUNT(*) AS num
+    FROM CTE 
+    GROUP BY requester_id
 )
-SELECT id, COUNT(id) AS num
-FROM CTE 
-GROUP BY id
-ORDER BY COUNT(id) DESC
-LIMIT 1;
+SELECT id, num
+FROM CTE2
+WHERE num = (SELECT MAX(num) FROM CTE2);
+
