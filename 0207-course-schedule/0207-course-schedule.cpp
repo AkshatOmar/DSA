1659dee278129456aa943519b0f25c44e3b3f059
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2026-06-11 14:24:53 +0530

    Time: 15 ms (9.92%), Space: 20.1 MB (24.91%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,35 +1,31 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>>&graph,int node,vector<int>&vis,vector<int>&path,int numCourses) {
-        vis[node] = true;
-        path[node] = true;
-        for(auto it : graph[node]) {
-            if(!vis[it]) {
-                if(dfs(graph,it,vis,path,numCourses)) return true;
-            }
-            else if(path[it]) {
-                return true;
-            }
-        }
-        path[node] = false;
-        return false;
-    }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>>graph(numCourses);
-        //build graph
+        vector<int>indegree(numCourses,0);
         for(auto it : prerequisites) {
             graph[it[0]].push_back(it[1]);
+            indegree[it[1]]++;
         }
-        vector<int>vis(numCourses,false);
-        vector<int>path(numCourses,false);
+        
+        queue<int>q;
         for(int i = 0;i<numCourses;i++) {
-            for(auto it : graph[i]) {
-                if(!vis[it]) {
-                    if(dfs(graph,it,vis,path,numCourses)) return false;
-                }
-                
+            if(indegree[i] == 0) {
+                q.push(i);
             }
         }
-        return true;
-    } 
+        int cnt = 0;
+        while(!q.empty()) {
+            int node = q.front();
+            q.pop();
+            cnt++;
+            for(auto it : graph[node]) {
+                indegree[it]--;
+                if(indegree[it] == 0) {
+                    q.push(it);
+                }
+            }
+        }
+        return cnt == numCourses;
+    }
 };
