6451ba4eff6159d279f418a6875cec954dc7287f
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2026-06-15 20:52:38 +0530

    Time: 7 ms (33.53%), Space: 19.3 MB (12.56%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,34 +1,35 @@
 class Solution {
 public:
-    bool dfs(int node,vector<vector<int>>&graph,vector<int>&vis,vector<int>&path,stack<int>&st) {
+    bool dfs(int node,vector<vector<int>>&graph,int numCourses, vector<int>&vis,vector<int>&path,stack<int>&st) {
         vis[node] = 1;
         path[node] = 1;
-        for(auto adjNode : graph[node]) {
-            if(vis[adjNode] == 0) {
-                if(dfs(adjNode,graph,vis,path,st)){
-                    return true;
-                }
-                
-            }else if(path[adjNode] ==1) {
+        for(auto it : graph[node]) {
+            if(vis[it] == 0) {
+                if(dfs(it,graph,numCourses,vis,path,st))return true;
+            }
+            else if(path[it] == 1){
                 return true;
             }
         }
         path[node] = 0;
         st.push(node);
         return false;
+        
     }
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>>graph(numCourses);
-        for(auto &it: prerequisites) {
+        for(auto it : prerequisites) {
             graph[it[1]].push_back(it[0]);
         }
         vector<int>vis(numCourses,0);
         vector<int>path(numCourses,0);
         stack<int>st;
         for(int i = 0;i<numCourses;i++) {
+            
             if(vis[i] == 0) {
-                if(dfs(i,graph,vis,path,st)) return {};
+                if(dfs(i,graph,numCourses,vis,path,st))return {};
             }
+            
         }
         vector<int>ans;
         while(!st.empty()) {
