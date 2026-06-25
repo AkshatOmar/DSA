e71a7c19e87d58958ac90b1f2cf468cc7af2f5a7
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2026-06-26 02:51:25 +0530

    Time: 3 ms (27.44%), Space: 17.1 MB (40.44%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,51 +1,53 @@
 class Solution {
 public:
-    vector<vector<int>>directions{{-1,0},{1,0},{0,1},{0,-1}};
-    bool isValid(int i, int j, int n, int m) {
-        return (i >= 0 && j >= 0 && i<n && j<m);
-    }
+    vector<vector<int>>directions{{-1,0},{1,0},{0,-1},{0,1}};
     int orangesRotting(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        int fresh = 0;
+        //queue to perform bfs
         queue<pair<int,int>>q;
+        int cnt = 0;
+        int freshOrg = 0;
         for(int i = 0;i<n;i++) {
             for(int j = 0;j<m;j++) {
                 if(grid[i][j] == 2) {
                     q.push({i,j});
-                }else if(grid[i][j] == 1) {
-                    fresh++;
+                }
+                else if(grid[i][j] == 1) {
+                    freshOrg++;
                 }
             }
         }
-        int ans = 0;
-        if(fresh == 0) return ans;
+        if(freshOrg == 0) return 0;
+        //Multisrc bfs
         while(!q.empty()) {
             int size = q.size();
-            for(int k =0 ;k<size;k++) {
+            for(int k = 0;k<size;k++) {
                 int i = q.front().first;
                 int j = q.front().second;
                 q.pop();
-
                 for(auto &dir : directions) {
-                    int new_i = i+dir[0];
-                    int new_j = j + dir[1];
-                    
-                    if(isValid(new_i,new_j,n,m)) {
+                    int new_i = dir[0] + i;
+                    int new_j = dir[1] + j;
+                    if(new_i >= 0 && new_i < n && new_j >= 0 && new_j < m) {
                         if(grid[new_i][new_j] == 1) {
-                            
                             grid[new_i][new_j] = 2;
-                            q.push({new_i, new_j});
-                            fresh--;
+                            q.push({new_i,new_j});
                         }
+
                     }
-                    
                 }
-                
             }
-            ans++;
+            cnt++;
         }
+        for(int i = 0;i<n;i++) {
+            for(int j = 0;j<m;j++) {
+                if(grid[i][j] == 1) {
+                    return -1;
+                }
+            }
+        }
+        return cnt-1; //-1 coz the last interation of while loop will increase the count without rotting orange
         
-        return fresh == 0 ? ans-1 : -1;
     }
 };
