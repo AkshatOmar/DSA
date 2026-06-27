83ab8be4fc4fc137bbdc4190fb2d8cd57506f31d
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2026-06-27 18:03:36 +0530

    Time: 4 ms (5.81%), Space: 18.7 MB (9.02%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
     vector<vector<int>>directions{{-1,0},{1,0},{0,-1},{0,1}};
-    bool isSafe(vector<vector<int>>&image, int i,int j, int n, int m) {
-        return (i>=0 && i<n && j>=0 && j<m);
-    }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        
         int n = image.size();
         int m = image[0].size();
-        int tgt = image[sr][sc];
-        if(tgt == color) return image;
+        int ognl = image[sr][sc];
+        if(ognl == color) return image;
         image[sr][sc] = color;
         queue<pair<int,int>>q;
         q.push({sr,sc});
         while(!q.empty()) {
-            int sz = q.size();
-            for(int k = 0;k<sz;k++) {
-                auto it = q.front();
-                q.pop();
-                int i = it.first;
-                int j = it.second;
-                for(auto& dir : directions) {
-                    int new_i = i+dir[0];
-                    int new_j = j + dir[1];
-                    if(isSafe(image,new_i,new_j,n,m) && image[new_i][new_j] == tgt) {
+            int i = q.front().first;
+            int j = q.front().second;
+            q.pop();
+            for(auto &dir : directions) {
+                int new_i = dir[0]+i;
+                int new_j = dir[1]+j;
+                
+                if(new_i>=0 && new_j>=0 && new_i<n && new_j<m) {
+                    if(image[new_i][new_j] == ognl) {
                         image[new_i][new_j] = color;
                         q.push({new_i,new_j});
                     }
                 }
             }
-        }   
+        }
         return image;
     }
 };
