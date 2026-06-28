32db040c49cce33636f4899eeca38fd6a28d862d
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2026-06-28 12:16:11 +0530

    Time: 3 ms (53.55%), Space: 28.3 MB (54.67%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
-    vector<vector<int>>directions{{-1,0},{1,0},{0,-1},{0,1}};
-    int dfs(vector<vector<int>>&grid, int i, int j, int n, int m) {
-
-        if(i < 0 || i >= n || j < 0 || j >= m || grid[i][j] != 1) {
+vector<vector<int>>directions{{-1,0},{1,0},{0,1},{0,-1}};
+    int dfs(vector<vector<int>>&grid,int i, int j, int n, int m) {
+        if(i<0 || j<0 || i>=n || j>=m || grid[i][j] == 0) {
             return 0;
         }
-        grid[i][j] = 2;
-        return 1+ dfs(grid,i-1,j,n,m)+dfs(grid,i+1,j,n,m)+dfs(grid,i,j-1,n,m)+dfs(grid,i,j+1,n,m);
+        
+        grid[i][j] = 0;
+        int area= 1;
+        for(auto &dir: directions) {
+            area+=dfs(grid,dir[0]+i,dir[1]+j,n,m);
+        }
+        return area;
     }
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        
         int maxArea = 0;
         for(int i = 0;i<n;i++) {
             for(int j = 0;j<m;j++) {
@@ -20,7 +23,7 @@ public:
                     maxArea = max(dfs(grid,i,j,n,m),maxArea);
                 }
             }
-        }
+        } 
         return maxArea;
     }
 };
