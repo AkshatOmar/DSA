3f75149792907fa6fb79705f15d6778f3dbba5ca
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2026-07-10 06:18:04 +0530

    Time: 763 ms (5.01%), Space: 55.6 MB (7.96%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,12 +1,19 @@
 class Solution {
 public:
+    bool helper(vector<int>&nums,int i, int n,vector<int>&dp) {
+        
+        if(i == n-1) return true;
+        if(i>=n || nums[i] == 0) return false;
+        if(dp[i] != -1) return dp[i];
+
+        for(int j = 1;j<=nums[i] && i+j < n;j++) {
+            if(helper(nums,i+j,n,dp)) return dp[i+j] = true;
+        }
+        return dp[i] = false;
+    }
     bool canJump(vector<int>& nums) {
         int n = nums.size();
-        int curr = 0, maxIdx = 0;
-        for(int i = 0;i<n;i++) {
-            if(i>maxIdx) return false;
-            maxIdx = max(maxIdx, i+nums[i]);
-        }
-        return true;
+        vector<int>dp(n,-1);
+        return helper(nums,0,n,dp);
     }
 };
