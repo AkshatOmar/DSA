4206cfab74eb323b0c46daed8e38db4e1900cab0
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2026-08-06 17:05:44 +0530

    Time: 11 ms (22.34%), Space: 19.6 MB (21.31%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
-    // bool helper(vector<int>&piles, int n, int i, int j) {
-    //     left = piles[i] + helper(piles,n,i+1,j);
-    //     right = piles[j]+helper(piles,n,i,j+1);
-
-
-    // }
+    int helper(vector<int>&piles, int n, int i, int j,vector<vector<int>>&dp) {
+        if(i>j) {
+            return 0;
+        }
+        if(dp[i][j] != -1) return dp[i][j];
+        int take_i = piles[i] + min(helper(piles,n,i+2,j,dp),helper(piles,n,i+1,j-1,dp));
+        int take_j = piles[j] + min(helper(piles,n,i+1,j-1,dp),helper(piles,n,i,j-2,dp));
+        return dp[i][j] = max(take_i,take_j);
+    }
     bool stoneGame(vector<int>& piles) {
         int n = piles.size();
-        //helper(piles, n, 0,n-1);
-        return true;
+        int sum = 0;
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        for(int i = 0;i<n;i++) {
+            sum += piles[i];
+        }
+        int alice =  helper(piles, n, 0,n-1,dp);
+        return (alice > sum/2);
     }
 };
