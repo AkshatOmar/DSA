a53873fffb0092dec5c5b407a200a544161eb33d
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2026-08-06 16:03:42 +0530

    Time: 0 ms (100%), Space: 10.6 MB (42.79%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    // bool helper(vector<int>&piles, int n, int i, int j) {
+    //     left = piles[i] + helper(piles,n,i+1,j);
+    //     right = piles[j]+helper(piles,n,i,j+1);
+
+
+    // }
+    bool stoneGame(vector<int>& piles) {
+        int n = piles.size();
+        //helper(piles, n, 0,n-1);
+        return true;
+    }
+};
