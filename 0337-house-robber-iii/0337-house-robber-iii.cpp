9064a80dc0827a0b1b702549dfc50ba68d450d50
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2026-08-25 03:08:52 +0530

    Time: 0 ms (100%), Space: 22.3 MB (89.35%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.cpp
+++ 0337-house-robber-iii/0337-house-robber-iii.cpp
@@ -13,16 +13,14 @@ class Solution {
 public:
     pair<int,int> helper(TreeNode* root) {
         if(root == NULL) return {0,0};
-
         auto left = helper(root->left);
         auto right = helper(root->right);
-
         int withRoot = root->val + left.second + right.second;
         int withoutRoot = max(left.first,left.second) + max(right.first,right.second);
-        return {withRoot,withoutRoot};
+        return {withRoot,withoutRoot}; 
     }
     int rob(TreeNode* root) {
-        pair<int,int> p = helper(root);
-        return max(p.first,p.second);
+        auto ans = helper(root);
+        return max(ans.first,ans.second);
     }
 };
