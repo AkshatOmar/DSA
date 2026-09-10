765f07db0e64f25279a6b3bf24220035c9974e2e
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2026-09-11 02:06:16 +0530

    Time: 23 ms (83.39%), Space: 16.4 MB (70.16%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,40 +1,34 @@
 class Solution {
 public:
-    vector<vector<int>>directions{{-1,0},{1,0},{0,-1},{0,1}};
-    void bfs(vector<vector<char>>&grid,int i, int j, int n, int m) {
-        queue<pair<int,int>>q;
-        q.push({i,j});
-        while(!q.empty()) {
-            int r = q.front().first;
-            int c = q.front().second;
-            q.pop();
-            
-            for(auto &dir : directions) {
-                int nr = dir[0]+r;
-                int nc = dir[1] + c;
-                if(nr>=0 && nc>=0 && nr<n && nc<m && grid[nr][nc] != '#') {
-                    if(grid[nr][nc] == '1') {
-                        grid[nr][nc] = '#';
-                        q.push({nr,nc});
-                    }
-                    
-                }
+int n,m;
+    vector<vector<int>>directions{{-1,0},{0,-1},{0,1},{1,0}};
+    void dfs(vector<vector<char>>&grid, int i,int j) {
+        grid[i][j] = '#';
+        for(auto &dir : directions) {
+            int new_i = dir[0]+i;
+            int new_j = dir[1]+j;
+            if(new_i>=0 && new_j >=0 && new_i <n &&new_j <m && grid[new_i][new_j] == '1' && grid[new_i][new_j] != '#') {
+                dfs(grid,new_i,new_j);
             }
+            
         }
     }
     int numIslands(vector<vector<char>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
         int islands = 0;
+         n = grid.size();
+         m = grid[0].size();
+        
         for(int i = 0;i<n;i++) {
             for(int j = 0;j<m;j++) {
                 if(grid[i][j] == '1') {
-                    bfs(grid,i,j,n,m);
-                    grid[i][j] = '#';
                     islands++;
+                    dfs(grid,i,j);
+                    
                 }
+                
             }
         }
+        
         return islands;
     }
 };
