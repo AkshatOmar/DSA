7c923cfccb0a3c3326302c5f2b28a3d96a107d78
Akshat Omar <168012064+AkshatOmar@users.noreply.github.com>
2026-09-24 18:27:36 +0530

    Time: 4 ms (14.07%), Space: 16.9 MB (44.84%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
-    vector<vector<int>>directions{{-1,0},{1,0},{0,-1},{0,1}};
     int orangesRotting(vector<vector<int>>& grid) {
-        int n = grid.size();
+        int n  = grid.size();
         int m = grid[0].size();
-        //queue to perform bfs
         queue<pair<int,int>>q;
-        int cnt = 0;
-        int freshOrg = 0;
+        int minutes = 0;
+        int fresh = 0;
         for(int i = 0;i<n;i++) {
             for(int j = 0;j<m;j++) {
                 if(grid[i][j] == 2) {
                     q.push({i,j});
                 }
                 else if(grid[i][j] == 1) {
-                    freshOrg++;
+                    fresh++;
                 }
             }
         }
-        if(freshOrg == 0) return 0;
-        //Multisrc bfs
+        if(fresh == 0) return 0;
+        vector<vector<int>>directions{{-1,0},{1,0},{0,-1},{0,1}};
         while(!q.empty()) {
             int size = q.size();
             for(int k = 0;k<size;k++) {
@@ -27,18 +25,17 @@ public:
                 int j = q.front().second;
                 q.pop();
                 for(auto &dir : directions) {
-                    int new_i = dir[0] + i;
-                    int new_j = dir[1] + j;
-                    if(new_i >= 0 && new_i < n && new_j >= 0 && new_j < m) {
-                        if(grid[new_i][new_j] == 1) {
-                            grid[new_i][new_j] = 2;
-                            q.push({new_i,new_j});
-                        }
-
+                    int new_i = i + dir[0];
+                    int new_j = j + dir[1];
+                    if(new_i >= 0 && new_i <n && new_j>=0 && new_j<m && grid[new_i][new_j] == 1) {
+                        grid[new_i][new_j] = 2;
+                        q.push({new_i,new_j});
+                        
                     }
                 }
             }
-            cnt++;
+            
+            minutes++;
         }
         for(int i = 0;i<n;i++) {
             for(int j = 0;j<m;j++) {
@@ -47,7 +44,6 @@ public:
                 }
             }
         }
-        return cnt-1; //-1 coz the last interation of while loop will increase the count without rotting orange
-        
+        return minutes-1;
     }
 };
